e462a540447c12f16db1d03e3750dedd884cc37b
leelk <karunarathne1995@gmail.com>
2023-01-20 18:01:03 +0530

    Adding Web Client

--- order-service/src/main/java/me/leel/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/me/leel/orderservice/config/WebClientConfig.java
@@ -1,8 +1,11 @@
 package me.leel.orderservice.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebClientConfig {
 
+    @Bean
+    public WebClient
 }
