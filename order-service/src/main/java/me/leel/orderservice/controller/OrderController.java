aa5e5bf72d1110084667b5525137955644c08559
leelk <karunarathne1995@gmail.com>
2023-02-03 22:55:08 +0530

    Enable Circuit breaker

--- order-service/src/main/java/me/leel/orderservice/controller/OrderController.java
+++ order-service/src/main/java/me/leel/orderservice/controller/OrderController.java
@@ -1,5 +1,6 @@
 package me.leel.orderservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import me.leel.orderservice.dto.OrderRequest;
 import me.leel.orderservice.service.OrderService;
@@ -15,9 +16,14 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully.";
+    }
+
+    private String fallBackMethod(OrderRequest orderRequest, RuntimeException e){
+        return "Oops.. Something went wrong. Please place order after some times..";
     }
 }
 
