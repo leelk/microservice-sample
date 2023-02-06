0c4697f5e59979b7b012d82292b691be5635ba26
leelk <karunarathne1995@gmail.com>
2023-02-06 08:12:23 +0530

    Add retry for resilen4J

--- order-service/src/main/java/me/leel/orderservice/controller/OrderController.java
+++ order-service/src/main/java/me/leel/orderservice/controller/OrderController.java
@@ -1,6 +1,7 @@
 package me.leel.orderservice.controller;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +23,7 @@ public class OrderController {
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallBackMethod")
     @TimeLimiter(name = "inventory")
+    @Retry(name = "inventory")
     public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
         return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
